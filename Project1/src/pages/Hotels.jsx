b4ef9ef37056de31eb83a3c980fbb19dc6d5cea4
gargparag712 <gargparag712@gmail.com>
2026-05-08 12:33:53 +0530

    Update Hotels.jsx

--- Project1/src/pages/Hotels.jsx
+++ Project1/src/pages/Hotels.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import travelData from "./travelData";
+import travelData from "../assets/travelData";
 
 const Hotels = () => {
   const city = localStorage.getItem("city") || "Delhi";
